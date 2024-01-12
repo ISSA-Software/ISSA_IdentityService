4b1cda7ea686b0d1dc8290683dbe2f96c94ee4aa
Nero <67089844+k-nero@users.noreply.github.com>
2024-01-12 15:16:28 +0700

    editor config

--- src/ISSA_IdentityService.Core/Utils/SystemHelper.cs
+++ src/ISSA_IdentityService.Core/Utils/SystemHelper.cs
@@ -4,7 +4,7 @@ using ISSA_IdentityService.Core.Config;
 namespace ISSA_IdentityService.Core.Utils;
 public class SystemHelper
 {
-    public static SystemSettingModel Setting => SystemSettingModel.Instance;
-    public static IConfiguration Configs => SystemSettingModel.Configs;
-    public static string? AppDb => SystemSettingModel.Configs.GetConnectionString("DefaultConnection");
+    public static SystemSettingModel? Setting => SystemSettingModel.Instance;
+    public static IConfiguration? Configs => SystemSettingModel.Configs;
+    public static string? AppDb => SystemSettingModel.Configs?.GetConnectionString("DefaultConnection");
 }
